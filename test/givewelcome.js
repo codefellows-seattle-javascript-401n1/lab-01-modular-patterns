220bd26c8c58dc9f16be964ca1d1beebce0dc965
Andrew Watson <andrewpeterwatson@gmail.com>
2016-04-28 18:04:01 -0700

    created greet funciton and tested to passing

---
+++ test/givewelcome.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const greet = require(__dirname + '/../lib/welcome');
+const assert = require('assert');
+
+
+describe('Test of module lib/welcome.js', function() {
+  describe('Testing funciton welcome', function() {
+    it('Should return Hello my friend', function() {
+      assert.equal(greet(), 'Hello my friend');
+    });
+  });
+});
