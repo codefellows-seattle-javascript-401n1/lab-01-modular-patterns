6b3d3a66f83437d9c7401838cf42477e5e72fd5d
Andrew Watson <andrewpeterwatson@gmail.com>
2016-04-28 18:33:25 -0700

    moving all commits

--- test/givewelcome.js
+++ test/givewelcome.js
@@ -3,7 +3,6 @@
 const greet = require(__dirname + '/../lib/welcome');
 const assert = require('assert');
 
-
 describe('Test of module lib/welcome.js', function() {
   describe('Testing funciton welcome', function() {
     it('Should return Hello my friend', function() {
