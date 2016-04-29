877ce3e743fbed0d1caf4503168d547706314246
Jacob Knaack <jacobknaack@Jacobs-MacBook-Pro-2.local>
2016-04-29 11:19:42 -0700

    updated test to check argument

--- lab-jacob/test/greet-test.js
+++ lab-jacob/test/greet-test.js
@@ -5,8 +5,9 @@ const assert = require('assert');
 
 describe('testing input', function() {
   describe('testing the greeting', function () {
-    it ('should return hello ' + process.argv[2], function() {
-      assert.equal(greet(process.argv[2]), 'hello ' + process.argv[2]);
+    it ('should assign "jacob" to argument and return hello jacob', function() {
+      process.argv[2] = 'jacob';
+      assert.equal(greet(process.argv[2]), 'hello jacob');
     });
   });
 });
