bd4385e23b5562a05cce2057e48e045d7f816fec
Andrew Watson <andrewpeterwatson@gmail.com>
2016-05-01 11:10:27 -0700

    created gulp file and dependencies

--- test/givewelcome.js
+++ test/givewelcome.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const greet = require(__dirname + '/../lib/welcome');
+const greet = require(__dirname + '/../index');
 const assert = require('assert');
 
 describe('Test of module lib/welcome.js', function() {
   describe('Testing funciton welcome', function() {
     it('Should return Hello Andrew', function() {
-      assert.equal(greet(), 'Hello Andrew');
+      assert.equal(greet(), 'hello andrew');
     });
   });
 });
